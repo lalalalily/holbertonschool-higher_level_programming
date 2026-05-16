6ed0f9e2125c422d489c642586108ee9d588f8b4
lalalalily <ranaarasulzade@gmail.com>
2026-05-16 17:21:37 +0000

    task

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,5 +5,5 @@ def max_integer(my_list=[]):
     max_val = my_list[0]
     for num in my_list:
         if num > max_val:
-            max_val = num        
+            max_val = num
     return max_val
