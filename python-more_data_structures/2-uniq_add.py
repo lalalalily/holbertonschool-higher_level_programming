c358294978ad98c6f3fb58556805f18048ecfffd
lalalalily <ranaarasulzade@gmail.com>
2026-05-16 20:01:07 +0000

    task

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    # set(my_list) removes all duplicates, sum() adds the remaining unique numbers
+    # set(my_list) removes all duplicates, 
+    # sum() adds the remaining unique numbers
     return sum(set(my_list))
