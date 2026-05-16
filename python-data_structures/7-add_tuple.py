0e93bb086a7e8c91a9ffa5e708ae2dff5ee37f0d
lalalalily <ranaarasulzade@gmail.com>
2026-05-16 17:33:16 +0000

    task

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -3,5 +3,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # Pad tuples or slice them to ensure we are working with at least 2 elements
     a = (tuple_a + (0, 0))[:2]
     b = (tuple_b + (0, 0))[:2]
-    
     return (a[0] + b[0], a[1] + b[1])
