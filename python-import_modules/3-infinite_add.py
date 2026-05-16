1d3478a558dd21be7f62305c32e6329bab3695a9
lalalalily <ranaarasulzade@gmail.com>
2026-05-16 16:31:28 +0000

    task

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    total = 0
+
+    # Skip the first element (script name) and loop through the rest
+    for arg in sys.argv[1:]:
+        total += int(arg)
+
+    print("{}".format(total))
