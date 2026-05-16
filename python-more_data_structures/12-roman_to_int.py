d16b5968a5deb73640cbfb548f155bc7f49694b7
lalalalily <ranaarasulzade@gmail.com>
2026-05-16 21:00:00 +0000

    12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -10,9 +10,8 @@ def roman_to_int(roman_string):
     length = len(roman_string)
     for i in range(length):
         current_val = roman_dict.get(roman_string[i], 0)
-        if (i + 1 < length
-            and roman_dict.get(roman_string[i + 1], 0) > current_val):
-                total -= current_val
+        if i + 1 < length and roman_dict.get(roman_string[i + 1], 0) > current_val:
+            total -= current_val
         else:
             total += current_val
     return total
